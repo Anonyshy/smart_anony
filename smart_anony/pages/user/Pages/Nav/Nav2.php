b09b0d4440a7363bae3b47a57a502c12351f2b45
Anonyshy <102345443+Anonyshy@users.noreply.github.com>
2023-07-08 12:43:30 +0530

    Add files via upload

--- smart_anony/pages/user/Pages/Nav/Nav2.php
+++ smart_anony/pages/user/Pages/Nav/Nav2.php
@@ -3,7 +3,8 @@
 <head>
 
 	<link rel="stylesheet" type="text/css" href="../../CSS/External.css">
-
+	
+	
 </head>
 
 <body bgcolor="#212121">
@@ -15,15 +16,14 @@
 			<a href="../Home_cont/Prod.php" target="main">Products</a> &nbsp;&nbsp;&nbsp;| &nbsp;&nbsp;
 			<a href="../About.php" target="main">About US</a> &nbsp;&nbsp;&nbsp;| &nbsp;
 			<a href="../Contact.php" target="main">Contact US</a>
+			
 		</font>
-		<a href="../../../Index.html" target="_parent" style="margin-left:450px;">Logout</a>
-		<a href="../Home_cont/cart.php" target="main" style="margin-left:30px;">My Cart</a>
+		
+		<a href="../Home_cont/cart.php" target="main" style="margin-left:330px;"> My Cart/Wishlist</a>
+			<a href="../../../Index.html" target="_parent" style="margin-left:20px;">Logout</a>
 
-		<form id="frmSrch" role="search">
-			<input type="search" id="srch" name="srch" placeholder="Search hear.."
-				aria-label="Search through site content">
-			<button>Search</button>
-		</form>
+
+
 
 
 	</h3>
