2d47e33c3a3d88e56be1a0a62a9e771e39e08016
Anonyshy <102345443+Anonyshy@users.noreply.github.com>
2023-08-28 21:32:02 +0530

    Delete smart_anony directory

--- smart_anony/pages/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $host = "localhost";
-  $dbusername = "root";
-  $dbpassword = "";
-  $dbname = "smart_anony";
-
-  // Create connection
-  $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
-?>
--- smart_anony/pages/user/Pages/logout.php
+++
@@ -1,3 +0,0 @@
-<?php
-header("location: ../Index.php?check=lgout");
-?>
